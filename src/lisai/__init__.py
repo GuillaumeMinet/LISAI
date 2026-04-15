ada3c6af4192d8743ff69b3643e2bc9cc3674abd
GuillaumeMinet <gemt38@gmail.com>
2026-04-15 12:11:11 +0200

    clear lisai api

---
+++ src/lisai/__init__.py
@@ -0,0 +1,3 @@
+from .api import train, train_from_dict, evaluate, apply_model
+
+__all__ = ["train", "train_from_dict", "evaluate", "apply_model"]
---
+++ src/lisai/api.py
@@ -0,0 +1,5 @@
+from lisai.training.run_training import (
+    run_training as train,
+    run_training_from_config_dict as train_from_dict,
+)
+from lisai.evaluation import run_evaluate as evaluate, run_apply_model as apply_model
